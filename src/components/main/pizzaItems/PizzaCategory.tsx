d24ca67862a739817ce4f6d69ed4b9f4458f36f4
lstnchDev <freest2334@gmail.com>
2022-11-22 20:38:14 +0300

    created css mainpizza menu

--- src/components/main/pizzaItems/PizzaCategory.tsx
+++ src/components/main/pizzaItems/PizzaCategory.tsx
@@ -1,17 +1,25 @@
-import { FC } from 'react';
-import Button from '../../../UI/Button';
+import { FC, useState } from 'react';
+import styles from "./styles/categories.module.scss"
 
 const dough = ['тонкое', 'традиционное']
 const size = ['26', '30', '40']
+
+
 const PizzaCategory: FC = ()=>{
-    
-    const doughSelector = dough.map(item=> <li className=''>{item}</li>)
-    const sizeSelector = size.map(item=> <li>{item}</li>)
+
+    const [activeStateDough, setActiveDough] = useState(false)
+    const [activeStateSize, setActiveSize] = useState(false)
+
+    const doughHandler = ()=>setActiveDough(true)
+    const sizeHandler = ()=>setActiveSize(true)
+
+    const doughSelector = dough.map(item=> <li onClick={doughHandler} className={styles.active}>{item}</li>)
+    const sizeSelector = size.map(item=> <li onClick={sizeHandler} className={styles.active}>{item}</li>)
 
     return(
-        <div className="category">
-            <ul>{doughSelector}</ul>
-            <ul>{sizeSelector}</ul>
+        <div className={styles.category}>
+            <ul className={styles.dough}>{doughSelector}</ul>
+            <ul className={styles.size}>{sizeSelector}</ul>
         </div>  
     )
 }
--- src/components/main/pizzaItems/PizzaItem.tsx
+++ src/components/main/pizzaItems/PizzaItem.tsx
@@ -2,18 +2,20 @@ import { FC } from 'react';
 import pizzaImg from "../../../icons/pizza.png"
 import Button from '../../../UI/Button';
 import PizzaCategory from './PizzaCategory';
+import styles from './styles/item.module.scss';
 
 const PizzaItem: FC = ()=>{
     const onClickHandler = ()=> console.log('click')
 
     return (
-        <li className="pizza">
-            <img src={pizzaImg} alt="" />
+        <li className={styles.pizza}>
+            <img className={styles.image} src={pizzaImg} alt="" />
             <h3>Чизбургер-пицца</h3>
             <PizzaCategory/>
-            <h4>от 395 ₽</h4>
-            <Button onClick={onClickHandler} title="Добавить"/>
-
+            <div className={styles.bottom}>
+                <h4>от 395 ₽</h4>
+                <Button onClick={onClickHandler} title="Добавить"/>
+            </div>
         </li>
     )
 }
